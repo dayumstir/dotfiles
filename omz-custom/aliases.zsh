b8a1707a94edcee3412287a9cc6d1676c35cb4c7
Dempster Tay <dempstertay123@gmail.com>
2026-08-20 12:28:45 +0800

    add brewup command

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -1,3 +1,9 @@
 alias c="clear"
 alias cbs="./gradlew clean build spotlessApply"
 alias cc="claude"
+
+# sudo -v caches credentials up front so the upgrade doesn't stall on a password prompt mid-run
+brewup() {
+  sudo -v || return 1
+  brew update && brew upgrade --greedy "$@"
+}
